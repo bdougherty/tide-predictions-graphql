7734d60bfec787d8aee055dda6592dd76f01fa50
Brad Dougherty <me@brad.is>
2018-06-02 18:00:10 -0400

    Add Latitude and Longitude scalars

---
+++ schema/scalars.js
@@ -0,0 +1,38 @@
+import { GraphQLScalarType } from 'graphql';
+
+const validateLongitude = (value) => {
+	value = parseFloat(value);
+
+	if (value > 90 || value < -90) {
+		throw new TypeError('Longitude must be between -90 and 90');
+	}
+
+	return value;
+};
+
+const validateLatitude = (value) => {
+	value = parseFloat(value);
+
+	if (value > 180 || value < -180) {
+		throw new TypeError('Latitude must be between -180 and 180');
+	}
+
+	return value;
+};
+
+export default {
+	Longitude: new GraphQLScalarType({
+		name: 'Longitude',
+		description: 'The longitude value of a coordinate. Must be a float between -90 and 90, inclusive.',
+		serialize: validateLongitude,
+		parseValue: validateLongitude,
+		parseLiteral: ({ value }) => validateLongitude(value)
+	}),
+	Latitude: new GraphQLScalarType({
+		name: 'Latitude',
+		description: 'The latitude value of a coordinate. Must be a float between -180 and 180, inclusive.',
+		serialize: validateLatitude,
+		parseValue: validateLatitude,
+		parseLiteral: ({ value }) => validateLatitude(value)
+	})
+};
