f3995bd36597718ea8f8d9c66906436261e22820
Brad Dougherty <me@brad.is>
2018-07-02 17:46:29 -0400

    Set cors headers on graphql request too

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ if (!process.env.ACCESS_CONTROL_ALLOW_ORIGIN) {
 	throw new Error('Must set ACCESS_CONTROL_ALLOW_ORIGIN environment variable.');
 }
 
+const originEnv = process.env.ACCESS_CONTROL_ALLOW_ORIGIN;
+const allowedOrigins = originEnv.split(',');
+
 const graphqlHandler = microGraphql({ schema });
 const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' });
 
@@ -18,27 +21,29 @@ const corsRoute = route('*', 'OPTIONS');
 const graphqlRoute = route('/graphql', ['GET', 'POST']);
 const graphiqlRoute = route('/graphiql', 'GET');
 
+const setCorsHeaders = (req, res) => {
+	const { origin } = req.headers;
+
+	res.setHeader('Access-Control-Allow-Methods', ['GET', 'POST'].join(','));
+	res.setHeader('Access-Control-Allow-Headers', ['X-Requested-With', 'Content-Type', 'Accept'].join(','));
+
+	if (originEnv === '*') {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+	}
+
+	if (allowedOrigins.indexOf(origin) !== -1) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+};
+
 export default (req, res) => {
 	if (corsRoute(req)) {
-		const originEnv = process.env.ACCESS_CONTROL_ALLOW_ORIGIN;
-		const allowedOrigins = originEnv.split(',');
-		const { origin } = req.headers;
-
-		res.setHeader('Access-Control-Allow-Methods', ['GET', 'POST'].join(','));
-		res.setHeader('Access-Control-Allow-Headers', ['X-Requested-With', 'Content-Type', 'Accept'].join(','));
-
-		if (originEnv === '*') {
-			res.setHeader('Access-Control-Allow-Origin', '*');
-		}
-
-		if (allowedOrigins.indexOf(origin) !== -1) {
-			res.setHeader('Access-Control-Allow-Origin', origin);
-		}
-
+		setCorsHeaders(req, res);
 		return '';
 	}
 
 	if (graphqlRoute(req)) {
+		setCorsHeaders(req, res);
 		return graphqlHandler(req, res);
 	}
 
